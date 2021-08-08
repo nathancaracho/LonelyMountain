48931e980b6b0f9a4c3551f057c5468665fe785c
Nathan Branco Caracho <nathan.caracho@hotmail.com>
2021-08-08 20:35:27 -0300

    create queue annotation and extention

---
+++ LonelyMountain.Src/Queue/Annotations.cs
@@ -0,0 +1,24 @@
+namespace LonelyMountain.Src.Queue
+{
+    public class Queue : System.Attribute
+    {
+        private readonly string _name;
+        public Queue(string name) => _name = name;
+        public static implicit operator string(Queue queue) => queue._name;
+    }
+
+    public class DlqQueue : Queue
+    {
+        public DlqQueue(string name) : base(name) { }
+    }
+
+    public class ActiveQueue : Queue
+    {
+        public ActiveQueue(string name) : base(name) { }
+    }
+
+    public class Topic : Queue
+    {
+        public Topic(string name) : base(name) { }
+    }
+}
---
+++ LonelyMountain.Src/Queue/QueueExtentions.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using LonelyMountain.Src.Consumer;
+
+namespace LonelyMountain.Src.Queue
+{
+
+    public static class QueueExtentions
+    {
+        /// <summary>
+        /// Get type of consumer ActiveQueue,DlqQueue or Topic.
+        /// </summary>
+        /// <param name="consumer">Annoted consumer</param>
+        /// <typeparam name="TMessage">consumer message type</typeparam>
+        /// <returns>Queue type</returns>
+        public static Queue GetConsumerType<TMessage>(this IConsumer<TMessage> consumer)
+        {
+            Queue queue;
+
+            var properties = consumer.GetType().GetCustomAttributes(typeof(Queue), true);
+            if (properties.Any() is false)
+                throw new InvalidOperationException("The consumer hasn't queue annotation; Use ActiveQueue, DlqQueue or Topic.");
+
+            queue = (properties.FirstOrDefault() as Queue);
+            if (string.IsNullOrEmpty(queue))
+                throw new ArgumentException("The Queue Name can't be null, undefined or empty.");
+
+            return queue;
+        }
+    }
+}
