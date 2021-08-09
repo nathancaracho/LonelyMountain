3b92a42265a29525ebe30fb3d84a2cd991c75d26
Nathan Branco Caracho <nathan.caracho@hotmail.com>
2021-08-08 21:39:13 -0300

    adding logs to subscribing supplier

--- LonelyMountain.Src/Subscriber/RabbitMqSubscriber.cs
+++ LonelyMountain.Src/Subscriber/RabbitMqSubscriber.cs
@@ -1,5 +1,6 @@
 using LonelyMountain.Src.Consumer;
 using LonelyMountain.Src.Queue;
+using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -9,18 +10,21 @@ namespace LonelyMountain.Src.Subscriber
     {
         private readonly IConsumer<TMessage> _consumer;
         private readonly IModel _channel;
+        private readonly ILogger _logger;
 
-        public RabbitMQSubscriber(IConsumer<TMessage> consumer)
+        public RabbitMQSubscriber(IConsumer<TMessage> consumer, ILogger<RabbitMQSubscriber<TMessage>> logger)
         {
             var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
             var connection = factory.CreateConnection();
             _consumer = consumer;
             _channel = connection.CreateModel();
+            _logger = logger;
         }
 
         public void Subscribe()
         {
             var consumer = _consumer.GetConsumerType();
+            _logger.LogInformation("Start subscribing {consumer} consumer", consumer);
 
             if (consumer is ActiveQueue)
                 ActiveQueueSubscribe(consumer);
@@ -28,13 +32,22 @@ namespace LonelyMountain.Src.Subscriber
 
         private void ActiveQueueSubscribe(string queueName)
         {
+
             var consumer = new EventingBasicConsumer(_channel);
             consumer.Received += async (_, eventArgument) =>
             {
+                _logger.LogInformation("The {consumer} consumer as triggered", queueName);
+
                 var result = await _consumer.ProcessMessage(eventArgument.Body.ToArray());
+
                 if (result.IsFailure)
+                {
                     _channel.BasicReject(eventArgument.DeliveryTag, false);
+                    _logger.LogError("An error was occurred when try processing {consumer}. Error: {error}", queueName, result.Error);
+                }
                 _channel.BasicNack(eventArgument.DeliveryTag, false, false);
+
+                _logger.LogInformation("The {consumer} was proccessed with success", queueName);
             };
 
             _channel.BasicConsume(queue: queueName,
