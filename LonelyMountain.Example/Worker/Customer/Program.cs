8b2574ccfd22a522023de87dd4fda4297b6c7d39
Nathan Branco Caracho <nathan.caracho@hotmail.com>
2021-08-11 22:34:46 -0300

    remove unnecessary program noise

--- LonelyMountain.Example/Worker/Customer/Program.cs
+++ LonelyMountain.Example/Worker/Customer/Program.cs
@@ -1,17 +1,10 @@
-﻿using System.IO;
-using FluentValidation;
+﻿using LonelyMountain.Example.Worker.Consumer;
 using LonelyMountain.Src;
 using LonelyMountain.Src.Ioc;
-using LonelyMountain.Src.Subscriber.Connections;
-using Microsoft.Extensions.DependencyInjection;
 
-namespace LonelyMountain.Example.Worker.Consumer
-{
-    class Program
-    {
-        static void Main(string[] args) =>
-            Bootstrap.Start((service, configuration) => service
-                                        .AddRabbitMQConsumer<CustomerMessage, CustomerValidator, CustomerConsumer>(configuration)
-                                        , args);
-    }
-}
+
+Bootstrap
+    .Start((service, configuration)
+            => service
+                .AddRabbitMQConsumer<CustomerMessage, CustomerValidator, CustomerConsumer>(configuration)
+            , args);
